e67c5fa75c3dc0e60ce1da04087f46d0d1c9fd8d
Jered Stevens <j.stevens8185@gmail.com>
2023-09-06 08:50:51 -0500

    add comments

--- Assignments/P01/main.go
+++ Assignments/P01/main.go
@@ -1,3 +1,13 @@
+//	FileName:	main.go
+//	Author:		Jered Stevens
+//	Date:		09/06/2023
+//	Class:		4143 - PLC - Griffin
+
+// Driver code for program 1
+//
+//	 Prints the mascot name for MSU
+//		by calling a function from
+//		a file in another folder
 package main
 
 import (
