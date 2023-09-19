4a80ac5f3b497144dab7ea3a599a1a12e2b434fd
Jered Stevens <j.stevens8185@gmail.com>
2023-09-19 11:42:28 -0500

    add constructor

--- Assignments/P02/imagemod/imageManipulator/imageManipulator.go
+++ Assignments/P02/imagemod/imageManipulator/imageManipulator.go
@@ -8,13 +8,25 @@ import (
 
 // ImageManipulator represents an image manipulation tool.
 type ImageManipulator struct {
-	Image *gg.Context
+	Image     *gg.Context
+	ImagePath string // Add a field to store the image path
 }
 
 // NewImageManipulator creates a new ImageManipulator instance.
 func NewImageManipulator(width, height int) *ImageManipulator {
 	img := gg.NewContext(width, height)
 	return &ImageManipulator{Image: img}
+}
+
+// NewImageManipulatorWithImage loads an existing image and creates an ImageManipulator instance.
+func NewImageManipulatorWithImage(imagePath string) (*ImageManipulator, error) {
+	img, err := gg.LoadImage(imagePath)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := gg.NewContextForImage(img)
+	return &ImageManipulator{Image: ctx, ImagePath: imagePath}, nil
 }
 
 // SaveToFile saves the manipulated image to a file.
--- Assignments/P02/imagemod/main.go
+++ Assignments/P02/imagemod/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"fmt"
+
 	// [module name]/[package name from subdirectory]
 	"myimageapp/imageManipulator"
 )
 
 func main() {
-	// Create imageManipulator instance
-	im := imageManipulator.NewImageManipulator(800, 600)
+	// Create an ImageManipulator instance with an existing image
+	im, err := imageManipulator.NewImageManipulatorWithImage("mustangs.jpg")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	// Draw rectangle
+	// Draw a rectangle
 	im.DrawRectangle(150, 50, 560, 411)
 
-	// Save image to a file
-	im.SaveToFile("rectangle.png")
+	// Save the modified image to a file
+	im.SaveToFile("mustangs.png")
 }
